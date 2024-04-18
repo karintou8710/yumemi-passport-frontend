368dbc5d90bb1894d09b0a9fd67ef3099ebb6456
karintou8710 <karintou8710@gmail.com>
2024-04-18 22:10:26 +0900

    [feat] Fontの追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,9 @@
+import '@fontsource/noto-sans-jp'
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
+
 import '@/app/globals.css'
 import RecoilProvider from '@/components/context/RecoilProvider'
 import Footer from '@/components/usecase/Footer'
